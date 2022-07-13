9e53e34ecd19cea6ae1a6a57a7d280d56159b058
ah7255703 <ah7255703@gmail.com>
2022-07-14 01:05:22 +0200

    first commit

---
+++ src/pages/Index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+const packages = [
+  'tailwindcss',
+  'redux-toolkit',
+  'react-redux',
+  'react-router-dom',
+  'react-icons',
+  'react-helmet'
+]
+
+
+
+function Index() {
+  return (
+    <div>
+      <div>
+        <center>my bolierplate</center>
+        <center>the packages</center>
+        <div>
+          {
+            packages.map((i,index)=>{return <span className='block p-1 my-3 mx-2 bg-[rgba(0,0,0,.5)] text-[#FFFF] rounded-lg'><code>{i}</code></span>})
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Index
