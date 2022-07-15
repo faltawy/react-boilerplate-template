3437c3f200e4127edd91fc97eb1fc6d6135f0aeb
ah7255703 <ah7255703@gmail.com>
2022-07-15 17:46:05 +0200

    mod

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Helmet,HelmetTags } from "react-helmet";
 import { Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact index element={<Index/>}/>
-      <Route path="*" element={<NotFound/>}/>
-    </Routes>)
+    <>
+      <Helmet>
+        <title>react app bolierblate</title>
+        <meta title="description" content="here is my react helmet"></meta>
+      </Helmet>
+      <Routes>
+        <Route path="/" exact index element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+    </>
+
+  )
 }
 
 export default App;
